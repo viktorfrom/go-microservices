9f57a4a0dbdf2c690781c69bfe8acb0b4635d492
viktorfrom <viktor.from91@gmail.com>
2022-04-22 13:31:52 +0200

    first docker build

---
+++ instruction.md
@@ -0,0 +1,2 @@
+1. docker build -t go-microservices:latest .
+2. docker run -dp 8080:8080 --name web go-microservices
--- main.go
+++ main.go
@@ -1,20 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	details "go-microservices/src/details"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Checking app health")
+	response := map[string]string{
+		"status":    "UP",
+		"timestamp": time.Now().String(),
+	}
+
+	json.NewEncoder(w).Encode(response)
+
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving the homepage")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "App is up and running")
+
+}
+
+func detailsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Fetching the details")
+	hostname, err := details.GetHostname()
+	if err != nil {
+		panic(err)
+	}
+
+	IP, err := details.GetIP()
+
+	fmt.Println(hostname, IP)
+
+	response := map[string]string{
+		"hostname": hostname,
+		"ip":       IP.String(),
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", handler).Methods("GET")
-	http.Handle("/", r)
-	http.ListenAndServe(":8080", r)
-}
+	//r.HandleFunc("/", handler).Methods("GET")
+	r.HandleFunc("/health", healthHandler)
+	r.HandleFunc("/details", detailsHandler)
+	r.HandleFunc("/", rootHandler)
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	http.Handle("/", r)
+
+	log.Println("Server has started")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
