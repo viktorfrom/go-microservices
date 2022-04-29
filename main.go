e9788073c9bc93d67dc59e8687f0218e806165de
viktorfrom <viktor.from91@gmail.com>
2022-04-29 15:43:19 +0200

    Add REST API to main.go

--- main.go
+++ main.go
@@ -1,67 +1,120 @@
 package main
 
 import (
+
+	//details "github.com/viktorfrom/go-microservices/details"
+
 	"encoding/json"
 	"fmt"
-
 	"log"
+	"math/rand"
 	"net/http"
-	"time"
-
-	details "github.com/viktorfrom/go-microservices/details"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+// Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Checking app health")
-	response := map[string]string{
-		"status":    "UP",
-		"timestamp": time.Now().String(),
+// Author Struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books var as a slice Book struct
+var books []Book
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Gets params
+	// Loop through books and find one with the id from the params
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
 	}
+	json.NewEncoder(w).Encode(&Book{})
+}
 
-	json.NewEncoder(w).Encode(response)
+// Add new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(100000000)) // Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
 
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+}
+
+// Delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving the homepage")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "App is up and running")
-
-}
-
-func detailsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Fetching the details")
-	hostname, err := details.GetHostname()
-	if err != nil {
-		panic(err)
-	}
-
-	IP, err := details.GetIP()
-
-	fmt.Println(hostname, IP)
-
-	response := map[string]string{
-		"hostname": hostname,
-		"ip":       IP.String(),
-	}
-
-	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
+	// Init Router
 	r := mux.NewRouter()
 
-	//r.HandleFunc("/", handler).Methods("GET")
-	r.HandleFunc("/health", healthHandler)
-	r.HandleFunc("/details", detailsHandler)
-	r.HandleFunc("/", rootHandler)
+	// Mock Data - $todo - implement DB
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One",
+		Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "847564", Title: "Book Two",
+		Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
+	// Route Handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	r.HandleFunc("/", rootHandler)
 	http.Handle("/", r)
 
 	log.Println("Server has started")
